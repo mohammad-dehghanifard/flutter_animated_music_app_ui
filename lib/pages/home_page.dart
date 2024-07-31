08a773b947b2ecb80e59547e2e1c480c850c1148
mohammad <m.deheghanifard@gmail.com>
2024-07-31 11:32:42 +0330

    completed home page ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animated_music_app/models/song.dart';
 import 'package:flutter_animated_music_app/widgets/song_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 1;
+  @override
   Widget build(BuildContext context) {
-    int count = 1;
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -22,14 +27,23 @@ class HomePage extends StatelessWidget {
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
                   ),
                   const Spacer(),
-                  Image.asset("assets/images/icon.png")
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        count == 1 ? count = 2 : count = 1;
+                      });
+                    },
+                      child: AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 600),
+                          child:  count == 2 ? Image.asset("assets/images/icon.png") : Image.asset("assets/images/list_icon.png" )
+                      ))
                 ],
               ),
             ),
             // grid view
             Expanded(
                 child: GridView.count(
-              childAspectRatio: 2.8,
+              childAspectRatio: count == 1 ? 2.8 : 1,
               crossAxisCount: count,
               children: List.generate(songsList.length, (index) => SongWidget(song: songsList[index],count: count,))
             ))
--- lib/widgets/song_widget.dart
+++ lib/widgets/song_widget.dart
@@ -50,7 +50,7 @@ class SongWidget extends StatelessWidget {
         ),
       )
     ],
-  );
+      );
   }
 
   Widget _buildVerticalSongItem(double imageSize) {
